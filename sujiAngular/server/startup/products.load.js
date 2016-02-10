03e3cf04b5a732fb39e70059c2b0bec23996cb65
Hyung Tak Jin <njir@naver.com>
2016-02-10 09:13:19 +0000

    Add sample data

--- sujiAngular/server/startup/products.load.js
+++ sujiAngular/server/startup/products.load.js
@@ -2,10 +2,20 @@ Meteor.startup(function() {
   if(Products.find().count() === 0) {
     var products = [
       {
-        'name': 'product 1'
+        'name': 'Tiramisu',
+        'price': '15',
+        'prime_cost': 5,
+        'tax_mode': true,
+        'barcode': null,
+        'category': 'CAKE'
       },
       {
-        'name': 'product 2'
+        'name': 'Espresso',
+        'price': '10',
+        'prime_cost': 2,
+        'tax_mode': false,
+        'barcode': null,
+        'category': 'COFFEE'
       }
     ];
     products.forEach(function(product) {
