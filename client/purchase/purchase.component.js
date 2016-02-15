2204683ab5c0bd1da18c6dce31b897296749aabc
김보운 <haioer@naver.com>
2016-02-15 19:22:45 +0900

    Purchase Log Success

--- client/purchase/purchase.component.js
+++ client/purchase/purchase.component.js
@@ -6,6 +6,6 @@ angular.module('suji-mr').directive('purchase', function () {
     return {
         restrict: 'E',
         templateUrl: 'client/purchase/purchase.html',
-        controllerAs: 'purchase'
+        controllerAs: 'PurchaseController'
     }
 });
