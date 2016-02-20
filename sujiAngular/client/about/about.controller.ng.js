f9fb8095ea982b636043bed9252b901859128fec
DongKyu Lee <ledgku@gmail.com>
2016-02-20 17:40:48 +0900

    integration-1

--- sujiAngular/client/about/about.controller.ng.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('sujiAngularApp')
-.controller('AboutCtrl', function($scope) {
-  $scope.viewName = 'About';
-});
--- sujiAngular/client/theme.ng.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-angular.module('sujiAngularApp')
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-  .primaryPalette('light-blue')
-  .accentPalette('blue');
-});
