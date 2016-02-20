41dd7bbc5ef9a5ea8fe0fa9e7188c6e37429a2d1
DongKyu Lee <ledgku@gmail.com>
2016-02-20 17:10:45 +0900

    Change folder structure

--- angular_bowoon/client/lib/app.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by BoWoon on 2016-02-12.
- */
-
-angular.module('suji-mr', ['angular-meteor', 'ui.router']);
--- angular_bowoon/model/menu.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by 보운 on 2016-02-12.
- */
-
-Menu = new Mongo.Collection("menu");
