7ea5d2b393691c891be0e257d5a95467f1ef97d0
Tak <njir@users.noreply.github.com>
2016-05-03 22:44:00 +0900

    chore: Delete comment

--- mobile/server/purchase.js
+++ mobile/server/purchase.js
@@ -1,17 +1,3 @@
 Meteor.publish("purchase", function(query, options) {
-  //if (!searchString) {
-  //  searchString = '';
-  //}
-  //var searchFor = {
-  //  '$regex': '.*' + (searchString || '') + '.*',
-  //  '$options': 'i'
-  //};
-  //
-  //return Purchase.find({
-  //    $or: [{
-  //      name: searchFor
-  //    }]
-  //  },
-  //  options);
   return Purchase.find(query, options);
 });
